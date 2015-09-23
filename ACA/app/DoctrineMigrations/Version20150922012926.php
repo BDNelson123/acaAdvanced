5f11d9692aed0e577ad44b6ee6ee8e26eacd6798
Sender <jhalesachs@gmail.com>
2015-09-22 19:34:35 -0500

    updated migration file

--- ACA/app/DoctrineMigrations/Version20150922012926.php
+++ ACA/app/DoctrineMigrations/Version20150922012926.php
@@ -17,8 +17,10 @@ class Version20150922012926 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, authkey VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('INSERT user(email, firstname, lastname, role, authkey) values("aaa@doom.com","Alan","Smithee","user","dfsfdsf43434");');
+        $this->addSql('INSERT user(email, firstname, lastname, role, authkey) values("bbb@whaa.com","M","N","user","4444jjxxo3043434");');
+        $this->addSql('INSERT user(email, firstname, lastname, role, authkey) values("ccc@caus.com","Franz","Schubert","admin","43434fffssf");');
     }
 
     /**
@@ -28,7 +30,6 @@ class Version20150922012926 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('DROP TABLE user');
     }
 }
