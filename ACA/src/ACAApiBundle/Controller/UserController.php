096dcf246e1423cfef46bcef7af40cc7b453dbb4
Sender <jhalesachs@gmail.com>
2015-09-24 20:45:26 -0500

    added show action, post action to UserController

--- ACA/src/ACAApiBundle/Controller/UserController.php
+++ ACA/src/ACAApiBundle/Controller/UserController.php
@@ -6,7 +6,6 @@ use ACAApiBundle\DBCommon;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-
 class UserController extends Controller
 {
     public function indexAction()
@@ -26,5 +25,37 @@ class UserController extends Controller
         ));
 
         return $response;
+    }
+
+    public function showAction($slug)
+    {
+        /**
+         * @var $db DBCommon
+         */
+        $db = new DBCommon('127.0.0.1', 'root', 'root', 'acaAdvanced', 3306);
+
+        $db->setQuery('SELECT * FROM user WHERE id=' .$slug. ';');
+        $db->query();
+        $result = $db->loadObjectList();
+
+        $response = new JsonResponse();
+        $response->setData(array(
+            'user' => $result
+        ));
+
+        return $response;
+    }
+
+    public function postAction()
+    {
+        $email = $_POST['email'];
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+
+        $db = new DBCommon('127.0.0.1', 'root', 'root', 'acaAdvanced', 3306);
+
+        $db->setQuery('INSERT INTO user(email, firstname, lastname, role) VALUES("'. $email. '", "' .$firstname. '", "' .$lastname. '", "user");');
+        $db->query();
+
     }
 }
