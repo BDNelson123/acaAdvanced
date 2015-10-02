9306f6d8eae19166da99559128bea83169a04d1a
Sender <jhalesachs@gmail.com>
2015-10-02 15:06:01 -0500

    RestService (cont.)
    Added some further precautions to Put and Post; still need the
    capability to deflect non-traversable arrays before foreach.

--- ACA/src/ACAApiBundle/Services/RestService.php
+++ ACA/src/ACAApiBundle/Services/RestService.php
@@ -67,13 +67,13 @@ class RestService {
         $query = 'INSERT INTO ' . $tableName . '(';
         foreach ($data as $fieldname => $fieldvalue)
         {
-            $query .= $fieldname .',';
+            $query .= str_replace(array(',','\\',';'), '', $fieldname) .',';
         }
         $query = rtrim($query, ',');
         $query .= ') values(';
         foreach ($data as $fieldname => $fieldvalue)
         {
-            $query .= '"' . $fieldvalue .'",';
+            $query .= '"' . str_replace(array(',','\\',';'), '', $fieldvalue) .'",';
         }
         $query = rtrim($query, ',');
         $query .= ');';
@@ -102,7 +102,7 @@ class RestService {
         $query = 'UPDATE ' . $tableName . ' SET ';
         foreach ($data as $fieldname => $fieldvalue)
         {
-            $query .=  $fieldname .'="'. $fieldvalue . '",';
+            $query .=  str_replace(array(',','\\',';'), '', $fieldname) .'="'. str_replace(array(',','\\',';'), '', $fieldvalue) . '",';
         }
         $query = rtrim($query, ',');
         $query .= ' WHERE id=' . $recordId .';';
