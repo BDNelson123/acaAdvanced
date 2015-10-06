5b24ac6fb480dfc136fe94e2f161ed25e4d1ea4c
vagrant <vagrant@acaadvanced.vm>
2015-10-06 01:21:24 +0000

    added basic validation for the Bid model

--- ACA/src/ACAApiBundle/Model/Bid.php
+++ ACA/src/ACAApiBundle/Model/Bid.php
@@ -2,16 +2,48 @@
 
 namespace ACAApiBundle\Model;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints as Assert;
+use \DateTime;
+
 /**
  * Class Bid
  * @package ACAApiBundle\Model\Bid
  */
 class Bid
 {
+    /**
+     * @var $id integer
+     */
     protected $id;
+
+    /**
+     * @var $userid integer
+     * @Assert\NotBlank()
+     */
     protected $userid;
+
+    /**
+     * @var $houseid integer
+     * @Assert\NotBlank()
+     */
     protected $houseid;
+
+    /**
+     * @var $bidamount integer
+     * @Assert\Range(
+     *      min = 1000,
+     *      max = 999999999,
+     *      minMessage = "Please enter a bid of at least ${{ limit }}.",
+     *      maxMessage = "Please be realistic with your bid.",
+     * )
+     */
     protected $bidamount;
+
+    /**
+     * @var $biddate datetime
+     * @Assert\DateTime()
+     */
     protected $biddate;
 
     /**
@@ -19,6 +51,38 @@ class Bid
      */
     public function __construct($id) {
         $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userid;
+    }
+
+    /**
+     * @param mixed $userid
+     */
+    public function setUserId($userid)
+    {
+        $this->userid = $userid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function gethouseid()
+    {
+        return $this->houseid;
+    }
+
+    /**
+     * @param mixed $houseid
+     */
+    public function setHouseId($houseid)
+    {
+        $this->houseid = $houseid;
     }
 
     /**
@@ -54,34 +118,50 @@ class Bid
     }
 
     /**
-     * @return mixed
+     * Validates a Bid with all required fields
+     * @param Request $request
+     * @return bool|array
      */
-    public function getUserId()
-    {
-        return $this->userid;
+    public static function validatePost(Request $request) {
+        $data = Bid::getDataFromRequest($request);
+
+        // If it's missing needed fields ...
+        if (empty($data['userid']) || empty($data['houseid']) || empty($data['bidamount'])) {
+            return false;
+        }
+
+        return $data;
     }
 
     /**
-     * @param mixed $userid
+     * Validates a Bid with any valid field
+     * @param Request $request
+     * @return bool|array
      */
-    public function setUserId($userid)
-    {
-        $this->userid = $userid;
+    public static function validatePut(Request $request) {
+        $data = Bid::getDataFromRequest($request);
+
+        // If it's missing needed fields ...
+        if (empty($data['userid']) && empty($data['houseid']) &&
+            empty($data['bidamount']) && empty($data['biddate'])) {
+            return false;
+        }
+
+        return $data;
     }
 
     /**
-     * @return mixed
+     * Gets an associative array out of a valid Json
+     * @param Request $request
+     * @return bool|array
      */
-    public function gethouseid()
-    {
-        return $this->houseid;
-    }
+    private static function getDataFromRequest(Request $request) {
+        $data = json_decode($request->getContent(), true);
 
-    /**
-     * @param mixed $houseid
-     */
-    public function setHouseId($houseid)
-    {
-        $this->houseid = $houseid;
+        // If the output of json_decode is not an array ...
+        if (gettype($data) !== 'array') { return false; }
+
+        // Passed all checks, return the contents of the request
+        return $data;
     }
 }
